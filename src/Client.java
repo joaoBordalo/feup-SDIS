ce973b97a9a8d0d3356df5e41c13c6125edcd919
lycantropus <mechanical.maggot.0@gmail.com>
2015-02-26 23:47:13 +0000

    update on client class
    added data members and started some prompt parsing

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,107 @@
 
 public class Client {
+	private
+	int host_name;
+	int port_number;
+	String oper;
+	String[] opnd;
+	
+	
+	public int getHost_name() {
+		return host_name;
+	}
+
+
+
+	public void setHost_name(int host_name) {
+		this.host_name = host_name;
+	}
+
+
+
+	public int getPort_number() {
+		return port_number;
+	}
+
+
+
+	public void setPort_number(int port_number) {
+		this.port_number = port_number;
+	}
+
+
+
+	public String getOper() {
+		return oper;
+	}
+
+
+
+	public void setOper(String oper) {
+		this.oper = oper;
+	}
+
+
+
+	public String[] getOpnd() {
+		return opnd;
+	}
+
+
+
+	public void setOpnd(String[] opnd) {
+		this.opnd = opnd;
+	}
+
+
+public static void main(String[] args)
+	{
+		if (args.length<4)
+		{
+			System.out.println("invalid usage. try: java Client <host_name> <port_number> <oper> <opnd>* ");
+		}
+		else
+		{
+			Client client = new Client();
+			
+			client.setHost_name(Integer.parseInt(args[0].toString()));
+			client.setPort_number(Integer.parseInt(args[1].toString()));
+			client.setOper(args[2].toString());
+			
+		 String[] temp = new String[args.length-3];
+		 
+		for(int i=0; i<temp.length; i++)
+		{
+			temp[i]=args[3+i].toString();
+		}
+	 
+		client.setOpnd(temp);
+			
+		
+		
+		System.out.print("Client Debug\n");
+		
+		System.out.print("Host Name:");
+		System.out.println(client.getHost_name());
+		
+		System.out.print("Port Number:");
+		System.out.println(client.getPort_number());
+		
+		System.out.print("Operation:");
+		System.out.println(client.getOper());
+		
+		System.out.print("Operation Args:");
+		
+		for(int i=0; i<temp.length; i++)
+		{
+		System.out.print(client.getOpnd()[i]+ " ");
+		}
+		System.out.print("\n");
+		//System.out.println(temp[0]);
+		}
+		
+	}
+
+
 
 }
