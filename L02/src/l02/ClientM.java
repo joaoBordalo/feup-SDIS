7d4d21d04fd6bb127cfac881b6bd11052e22de49
lycantropus <mechanical.maggot.0@gmail.com>
2015-03-25 00:05:37 +0000

    continuacao
    deu e deixou de dar.

--- L02/src/l02/ClientM.java
+++ L02/src/l02/ClientM.java
@@ -2,6 +2,7 @@ package l02;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
@@ -11,9 +12,10 @@ public class ClientM {
     final static String INET_ADDR = "224.0.0.3";
     final static int PORT = 8888;
 
+    //java client <mcast_addr> <mcast_port> <oper> <opnd> * 
     public static void main(String[] args) throws UnknownHostException {
         // Get the address that we are going to connect to.
-        InetAddress address = InetAddress.getByName(INET_ADDR);
+        InetAddress address = InetAddress.getByName(args[0]);
         
         // Create a buffer of bytes, which will be used to store
         // the incoming bytes containing the information from the server.
@@ -22,18 +24,24 @@ public class ClientM {
         
         // Create a new Multicast socket (that will allow other sockets/programs
         // to join it as well.
-        try (MulticastSocket clientSocket = new MulticastSocket(PORT)){
+        try (MulticastSocket clientSocket = new MulticastSocket(Integer.parseInt(args[1]))){
             //Joint the Multicast group.
             clientSocket.joinGroup(address);
      
-            while (true) {
+            
                 // Receive the information and print it.
                 DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
                 clientSocket.receive(msgPacket);
 
                 String msg = new String(buf, 0, buf.length);
-                System.out.println("Socket 1 received msg: " + msg);
-            }
+                System.out.println("service server port: " + msg);
+                
+              
+                
+                
+                msgPacket= new DatagramPacket(msg.getBytes(), msg.getBytes().length, address, 144);
+                clientSocket.send(msgPacket);
+            
         } catch (IOException ex) {
             ex.printStackTrace();
         }
--- L02/src/l02/ServerM.java
+++ L02/src/l02/ServerM.java
@@ -7,30 +7,50 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class ServerM {
-    
-    final static String INET_ADDR = "224.0.0.3";
-    final static int PORT = 8888;
 
-    public static void main(String[] args) throws UnknownHostException, InterruptedException {
-        // Get the address that we are going to connect to.
-        InetAddress addr = InetAddress.getByName(INET_ADDR);
-     
-        // Open a new DatagramSocket, which will be used to send the data.
-        try (DatagramSocket serverSocket = new DatagramSocket()) {
-            for (int i = 0; i < 5; i++) {
-                String msg = "Sent message no " + i;
+	final static String INET_ADDR = "224.0.0.3";
+	final static int PORT = 8888;
 
-                // Create a packet that will contain the data
-                // (in the form of bytes) and send it.
-                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
-                        msg.getBytes().length, addr, PORT);
-                serverSocket.send(msgPacket);
-     
-                System.out.println("Server sent packet with msg: " + msg);
-                Thread.sleep(500);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+	
+	//usage java Server <srvc_port> <mcast_addr> <mcast_port> 
+	public static void main(String[] args) throws UnknownHostException,
+			InterruptedException {
+		// Get the address that we are going to connect to.
+		InetAddress addr = InetAddress.getByName(args[1]);
+
+		// Open a new DatagramSocket, which will be used to send the data.
+		try (DatagramSocket serverSocket = new DatagramSocket()) {
+			
+			byte[] buf = new byte[256];
+			 while (true) {
+				String msg = args[0];
+
+				// Create a packet that will contain the data
+				// (in the form of bytes) and send it.
+				DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
+						msg.getBytes().length, addr, Integer.parseInt(args[2]));
+				
+				serverSocket.send(msgPacket);
+
+				System.out.println("sent service port " + msg);
+				Thread.sleep(500);
+				
+				
+		             // Receive the information and print it.
+		             msgPacket = new DatagramPacket(buf, buf.length);
+		             serverSocket.receive(msgPacket);
+
+		             msg = new String(buf, 0, buf.length);
+		             System.out.println("recebi: " + msg);
+		         }
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		
+		
+		
+		
+	}
 }
